cdafbe838a27d570594f3ff7e961a842416187a2
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-17 00:57:17 -0300

    service for dona graph

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,16 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
+import { ChartEvent, ChartType, ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DonaHttpComponent implements OnInit {
+  // Doughnut
+  public doughnutChartLabels: string[] = [];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [],
+        backgroundColor: ['#6405F0', '#0724E3', '#05A0F0'],
+      },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: {}[];
+  }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  constructor(private graficasService: GraficasService) {}
+
+  ngOnInit(): void {
+    this.graficasService
+      .getUsuariosRedesSocialesDonaData()
+      .subscribe((data) => (this.doughnutChartData = data));
+  }
 }
--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GraficasService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUsuariosRedesSociales() {
+    return this.http.get('http://localhost:3000/grafica');
+  }
+
+  getUsuariosRedesSocialesDonaData() {
+    return this.getUsuariosRedesSociales().pipe(
+      delay(1500),
+      map((data) => {
+        return {
+          labels: Object.keys(data),
+          datasets: [{ data: Object.values(data) }],
+        };
+      })
+    );
+  }
 }
